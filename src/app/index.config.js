d13a2b6481133b4e5a9dc69eb8d4d4aa7930686c
Hesham El Masry <heshamelmasry@outlook.com>
2017-10-30 14:54:51 +0200

    changing toaster duration time

--- src/app/index.config.js
+++ src/app/index.config.js
@@ -12,7 +12,7 @@
 
     // Set options third-party lib
     toastrConfig.allowHtml = true;
-    toastrConfig.timeOut = 3000;
+    toastrConfig.timeOut = 2000;
     toastrConfig.positionClass = 'toast-top-right';
     toastrConfig.preventDuplicates = true;
     toastrConfig.progressBar = true;
